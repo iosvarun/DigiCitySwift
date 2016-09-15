fdf0adc5861708b14b3a691f151924d322b34e6d
varunios <ios.varun@gmail.com>
2016-09-15 22:10:34 +0530

    added viewcontrollers

---
+++ DigiCity/AppUtility/AppUtility.swift
@@ -0,0 +1,13 @@
+//
+//  AppUtility.swift
+//  DigiCity
+//
+//  Created by Varun Kumar on 15/09/16.
+//  Copyright © 2016 Varun. All rights reserved.
+//
+
+import UIKit
+
+class AppUtility: NSObject {
+
+}
---
+++ DigiCity/ViewControllers/NotificationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NotificationViewController.swift
+//  DigiCity
+//
+//  Created by Varun Kumar on 15/09/16.
+//  Copyright © 2016 Varun. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
